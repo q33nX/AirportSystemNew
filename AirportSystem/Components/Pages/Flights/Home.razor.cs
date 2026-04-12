cc9d72ef7b907267435cd5621237774e1dbbded0
Dzmitry Danilau <dadv.by@gmail.com>
2026-04-12 13:10:47 +0300

    home page ui

---
+++ AirportSystem/Components/Pages/Flights/Home.razor.cs
@@ -0,0 +1,39 @@
+﻿using AirportSystem.Core.Entities;
+
+namespace AirportSystem.Components.Pages // Проверьте ваш namespace
+{
+    public partial class Home
+    {
+        // Перечисляемое для режимов поиска (избавляемся от строк)
+        private enum SearchMode { Roundtrip, MultiCity }
+
+        private SearchMode _currentMode = SearchMode.Roundtrip;
+
+        private List<FlightSchema> Segments = new()
+        {
+            new FlightSchema(),
+            new FlightSchema()
+        };
+
+        private void SetSearchMode(SearchMode mode)
+        {
+            _currentMode = mode;
+        }
+
+        private void AddSegment()
+        {
+            if (Segments.Count < 5)
+            {
+                Segments.Add(new FlightSchema());
+            }
+        }
+
+        private void RemoveSegment(int index)
+        {
+            if (Segments.Count > 2 && index < Segments.Count)
+            {
+                Segments.RemoveAt(index);
+            }
+        }
+    }
+}
